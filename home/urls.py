3ba86b081e3cc2704921e6ce29d2f7cfd319f2ee
sriyachirumamilla <144532525+sriyachirumamilla@users.noreply.github.com>
2025-08-13 11:22:27 +0530

    26844

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import *
+from .views import homepage
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
+    path('',homepage, name='homepage'),
 ]
